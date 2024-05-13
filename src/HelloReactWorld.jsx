5b33f7465367c1c5a94b6aaddde037ac8f6cbb0f
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-13 18:05:40 +0200

    Adding Bootstrap in React Using NPM

--- src/HelloReactWorld.jsx
+++ src/HelloReactWorld.jsx
@@ -1,7 +1,7 @@
 export default function HelloReactWorld() {
   return (
     <div>
-      <h1>Hello React World !</h1>
+      <h1 className="text-center">Hello React World !</h1>
     </div>
   )
 }
