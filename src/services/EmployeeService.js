ba6060ddb5135aa6002fc74f196e0c5f97ce8aff
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-22 08:40:39 +0200

    Calling Get All Employees REST API using Axios Library

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -3,5 +3,5 @@ import axios from "axios";
 // Calling Spring Boot Backend REST APIs for Employee Resources
 const REST_API_BASE_URL = "http://localhost:8080/api/v1/employees";
 
-// Calling Get All Employees REST API
+// Calling Get All Employees REST API using Axios Library
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
