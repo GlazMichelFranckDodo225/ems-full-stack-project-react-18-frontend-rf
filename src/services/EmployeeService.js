076db55b7cd1e4eeb3e454a9d08620e37d17a326
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-17 14:31:10 +0200

    Calling Add Employees REST API using Axios Library

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -5,3 +5,6 @@ const REST_API_BASE_URL = "http://localhost:8080/api/v1/employees";
 
 // Calling Get All Employees REST API using Axios Library
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
+
+// Calling Add Employees REST API using Axios Library
+export const createEmployee = () => axios.post(REST_API_BASE_URL, employee);
